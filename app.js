76e39c0de1275581a4fdeedc1b5bb466c2c8a176
dax-side <damolaadegbite0@gmail.com>
2025-01-11 03:50:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,44 +1,47 @@
-let weather ={
-  "apiKey": "ac8dec3a18a72ac4e78497827b1c649e",
-   fetchWeather: function(city) {
-     fetch(
-       "https://api.openweathermap.org/data/2.5/weather?q="
-        + city 
-        +"&units=metric&appid="
-        + this.apiKey
-       )
-       .then((response) => response.json())
-       .then((data) =>this.displayWeather(data));
-   },
-   displayWeather: function(data) {
-      const { name } = data;
-      const { icon, description} = data.weather[0];
-      const {temp, humidity} = data.main;
-      const {speed} = data.wind;
-      //console.log(name,icon,description,temp,humidity,speed);
-      document.querySelector(".city").innerText = "Weather in " + name;
-      document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
-      document.querySelector(".description").innerText = description;
-      document.querySelector(".temp").innerText = temp + "°C";
-      document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
-      document.querySelector(".wind").innerText = "Wind Speed" + speed +  " km";
+let weather = {
+  "apiKey": "6e8d16945397ed5d098c71f3b56b6034", // Updated API key
+  fetchWeather: function(city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q="
+      + city
+      + "&units=metric&appid="
+      + this.apiKey
+    )
+    .then((response) => response.json())
+    .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    
+    // Display weather information
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind Speed: " + speed + " km/h";
 
-      document.querySelector(".weather").classList.remove("loading");
-      // document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/? " + name + "')"
-   },
-   search: function () {
-    this.fetchWeather( document.querySelector(".search-bar").value);
-   }
+    // Remove loading state
+    document.querySelector(".weather").classList.remove("loading");
+  },
+  search: function() {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  }
 };
 
+// Event listeners for search button and input field
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
-})
+});
 
 document.querySelector(".search-bar").addEventListener("keyup", function (event) {
-  if(event.key == "Enter") {
+  if (event.key == "Enter") {
     weather.search();
   }
-})
+});
 
+// Fetch default weather for "Paris"
 weather.fetchWeather("Paris");
